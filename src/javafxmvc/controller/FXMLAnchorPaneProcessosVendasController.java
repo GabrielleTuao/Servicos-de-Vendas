6c02d27f4c65168f019a43a1ffc7422f461d277c
Gabrielle Tuao <gabrielle.donna2@gmail.com>
2020-05-31 07:05:07 -0300

    Quarto commit novas alterações

--- src/javafxmvc/controller/FXMLAnchorPaneProcessosVendasController.java
+++ src/javafxmvc/controller/FXMLAnchorPaneProcessosVendasController.java
@@ -1,25 +1,37 @@
 package javafxmvc.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafxmvc.model.dao.ItemDeVendaDAO;
 import javafxmvc.model.dao.ProdutoDAO;
 import javafxmvc.model.dao.VendaDAO;
 import javafxmvc.model.database.Database;
 import javafxmvc.model.database.DatabaseFactory;
+import javafxmvc.model.domain.ItemDeVenda;
+import javafxmvc.model.domain.Produto;
 import javafxmvc.model.domain.Venda;
 
 public class FXMLAnchorPaneProcessosVendasController implements Initializable {
@@ -103,5 +115,91 @@ public class FXMLAnchorPaneProcessosVendasController implements Initializable {
         tableViewVendas.setItems(observableListVendas);
     }
     
+     @FXML
+    public void handleButtonInserir() throws IOException , SQLException{
+         Venda venda = new Venda();
+        List<ItemDeVenda> listItensDeVenda = new ArrayList<>();
+        venda.setItensDeVenda(listItensDeVenda);
+        boolean buttonConfirmarClicked = showFXMLAnchorPaneProcessosVendasDialog(venda);
+        if (buttonConfirmarClicked) {
+            try {
+/*Significa que quando mandar inseir uma venda ele não irá inserir de fato na tabela, só vai inserir de fato 
+quando for chamar  o método commit, por se tratar de um obeto composto, eu só posso inserir uma venda se eu conseguir colocar
+ todos os itens de venda, para garantir se faz o commit no final*/
+                connection.setAutoCommit(false);              
+                vendaDAO.setConnection(connection);
+                vendaDAO.inserir(venda);
+                itemDeVendaDAO.setConnection(connection);
+                produtoDAO.setConnection(connection);
+                for (ItemDeVenda listItemDeVenda : venda.getItensDeVenda()) {
+                    Produto produto = listItemDeVenda.getProduto();
+                    listItemDeVenda.setVenda(vendaDAO.buscarUltimaVenda());
+                    itemDeVendaDAO.inserir(listItemDeVenda);
+                    produto.setQuantidade(produto.getQuantidade() - listItemDeVenda.getQuantidade());
+                    produtoDAO.alterar(produto);
+                }
+                connection.commit();
+                carregarTableViewVendas();
+            } catch (SQLException ex) {
+                try {
+                    connection.rollback();
+                } catch (SQLException ex1) {
+                    Logger.getLogger(FXMLAnchorPaneProcessosVendasController.class.getName()).log(Level.SEVERE, null, ex1);
+                }
+                Logger.getLogger(FXMLAnchorPaneProcessosVendasController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    @FXML
+    public void handleButtonAlterar() throws IOException {
+
+    }
+
+    @FXML
+    public void handleButtonRemover() throws IOException, SQLException {
+        Venda venda = tableViewVendas.getSelectionModel().getSelectedItem();
+        if (venda != null) {
+            connection.setAutoCommit(false);
+            vendaDAO.setConnection(connection);
+            itemDeVendaDAO.setConnection(connection);
+            produtoDAO.setConnection(connection);
+//primeiro tem que deletar a venda pra depois deletar os itens de venda
+            for (ItemDeVenda listItemDeVenda : venda.getItensDeVenda()) {
+                Produto produto = listItemDeVenda.getProduto();
+                produto.setQuantidade(produto.getQuantidade() + listItemDeVenda.getQuantidade());
+                produtoDAO.alterar(produto);
+                itemDeVendaDAO.remover(listItemDeVenda);
+            }
+            vendaDAO.remover(venda);
+            connection.commit();
+            carregarTableViewVendas();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Por favor, escolha uma venda na Tabela!");
+            alert.show();
+        }
+    }
+    
+    public boolean showFXMLAnchorPaneProcessosVendasDialog(Venda venda) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(FXMLAnchorPaneProcessosVendasDialogController.class.getResource("/javafxmvc/view/FXMLAnchorPaneProcessosVendasDialog.fxml"));
+        AnchorPane page = (AnchorPane) loader.load();
+        
+        //Criando um Estágio de Diálogo (Stage Dialog)
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle ("Registro de Vendas");
+        Scene scene  = new Scene(page);
+        dialogStage.setScene(scene);
+        
+        //Setando a Venda no Controller
+        FXMLAnchorPaneProcessosVendasDialogController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        controller.setVenda(venda);
+        
+        //Mostra dialog e espera até o usuário o feche
+        dialogStage.showAndWait();
+        return controller.isButtonConfirmarClicked();
+    }
     
 }
